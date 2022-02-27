be2e0ccdb0e00bc13fcfd118e4f9488014335246
Carlos Pérez Yáñez <charlieghost938@gmail.com>
2022-02-27 19:51:18 +0100

    Avance de las páginas de inicio, item y creacion de la de usuario aunque aun no tiene nada

--- Attempt1/src/main/java/com/SafeWebDev/attempt/Controllers/ControllerBasic.java
+++ Attempt1/src/main/java/com/SafeWebDev/attempt/Controllers/ControllerBasic.java
@@ -5,26 +5,35 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class ControllerBasic {
+    List<Item> items=new ArrayList<Item>();
+    public ControllerBasic(){
+        items.add(new Item("Boxers Hombre","XXL","Desgastado, dado de sí y manchado",10));
+        items.add(new Item("Bragas Mujer","XL","Desgastado, sucio",15));
+        items.add(new Item("Calcetin Blanco","L","Con agujeros, olor a esmegma",35));
+        items.add(new Item("Sujetador Mujer","92B","Hecho mierda",25));
+    }
 
     @GetMapping("")
     public String homePage(Model model){
-        model.addAttribute("items",getItems());
+        model.addAttribute("items",items);
         return "StartPage";
     }
 
-    public List<Item> getItems(){
-        List<Item> items=new ArrayList<Item>();
-        items.add(new Item("Boxers Hombre","XXL","Desgastado, dado de sí y manchado"));
-        items.add(new Item("Bragas Mujer","XL","Desgastado, sucio"));
-        items.add(new Item("Calcetin Blanco","L","Con agujeros, olor a esmegma"));
-        items.add(new Item("Sujetador Mujer","92B","Hecho mierda"));
+    @GetMapping("/item")
+    public String itemPage(Model model,@RequestParam int item){
+        model.addAttribute("item",items.get(item-1));
+        return "ItemPage";
+    }
 
-        return items;
+    @GetMapping("/usr")
+    public String usrPage(){
+        return "UsrPage";
     }
 }
--- Attempt1/src/main/java/com/SafeWebDev/attempt/Models/Item.java
+++ Attempt1/src/main/java/com/SafeWebDev/attempt/Models/Item.java
@@ -7,13 +7,15 @@ public class Item{
     private String nombre;
     private String talla;
     private String estado;
+    private float precio;
     private int stock=0;
-    private static long lastId=0;
+    private static long lastId=1;
 
-    public Item(String nombre, String talla, String estado) {
+    public Item(String nombre, String talla, String estado,float precio) {
         this.nombre = nombre;
         this.talla = talla;
         this.estado = estado;
+        this.precio=precio;
         id=lastId;
         lastId++;
     }

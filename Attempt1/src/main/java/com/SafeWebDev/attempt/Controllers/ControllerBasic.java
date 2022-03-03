50fadfaf61cf8ba88a9a6faf7ea35b2a1fd39553
jaimemedlop <68601868+jaimemedlop@users.noreply.github.com>
2022-03-03 13:00:33 +0100

    creacion de usuarios

--- Attempt1/src/main/java/com/SafeWebDev/attempt/Controllers/ControllerBasic.java
+++ Attempt1/src/main/java/com/SafeWebDev/attempt/Controllers/ControllerBasic.java
@@ -12,28 +12,40 @@ import java.util.List;
 
 @Controller
 public class ControllerBasic {
-    List<Item> items=new ArrayList<Item>();
-    public ControllerBasic(){
-        items.add(new Item("Boxers Hombre","XXL","Desgastado, dado de sí y manchado",10));
-        items.add(new Item("Bragas Mujer","XL","Desgastado, sucio",15));
-        items.add(new Item("Calcetin Blanco","L","Con agujeros, olor a esmegma",35));
-        items.add(new Item("Sujetador Mujer","92B","Hecho mierda",25));
+
+    List<Item> items = new ArrayList<Item>();
+
+    public ControllerBasic() {
+        items.add(new Item("Boxers Hombre", "XXL", "Desgastado, dado de sí y manchado", 10));
+        items.add(new Item("Bragas Mujer", "XL", "Desgastado, sucio", 15));
+        items.add(new Item("Calcetin Blanco", "L", "Con agujeros, olor a esmegma", 35));
+        items.add(new Item("Sujetador Mujer", "92B", "Hecho mierda", 25));
     }
 
     @GetMapping("")
-    public String homePage(Model model){
-        model.addAttribute("items",items);
+    public String homePage(Model model) {
+        model.addAttribute("items", items);
         return "StartPage";
     }
 
     @GetMapping("/item")
-    public String itemPage(Model model,@RequestParam int item){
-        model.addAttribute("item",items.get(item-1));
+    public String itemPage(Model model, @RequestParam int item) {
+        model.addAttribute("item", items.get(item - 1));
         return "ItemPage";
     }
 
     @GetMapping("/usr")
-    public String usrPage(){
+    public String usrPage() {
         return "UsrPage";
+    }
+
+    @GetMapping("/login")
+    public String logIn(){
+        return "LogIn";
+    }
+
+    @GetMapping("/createAccount")
+    public String createAccount(){
+        return "CreateAccount";
     }
 }
---
+++ Attempt1/src/main/java/com/SafeWebDev/attempt/Models/User.java
@@ -0,0 +1,17 @@
+package com.SafeWebDev.attempt.Models;
+
+public class User {
+
+    private String usuario;
+    private String correo;
+    private String password;
+    private boolean premium;
+    private String direccion;
+    private String datosBancarios;
+    private long id;
+
+
+    public User(String usuario, String correo, String password){
+
+    }
+}

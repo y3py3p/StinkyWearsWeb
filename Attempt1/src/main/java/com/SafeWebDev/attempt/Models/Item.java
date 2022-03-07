f6c414fdf0e785797075c67acedd0a417a193fd9
Carlos Pérez Yáñez <charlieghost938@gmail.com>
2022-03-07 14:33:16 +0100

    Arreglando la cagada

---
+++ Attempt1/src/main/java/com/SafeWebDev/attempt/Models/Item.java
@@ -0,0 +1,68 @@
+package com.SafeWebDev.attempt.Models;
+
+import java.util.*;
+
+public class Item{
+    private long id;
+    private String nombre;
+    private String talla;
+    private String estado;
+    private float precio;
+    private int stock=0;
+    private static long lastId=1;
+
+    public Item(String nombre, String talla, String estado,float precio) {
+        this.nombre = nombre;
+        this.talla = talla;
+        this.estado = estado;
+        this.precio=precio;
+        id=lastId;
+        lastId++;
+    }
+    public Item(){
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTalla() {
+        return talla;
+    }
+
+    public void setTalla(String talla) {
+        this.talla = talla;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", talla='" + talla + '\'' +
+                ", estado='" + estado + '\'' +
+                ", stock='" + stock + '\'' +
+                '}';
+    }
+}

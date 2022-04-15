83955977d74cbbc8a2a0c2da9ca44362a666db2e
y3py3p <syzygi.ale@gmail.com>
2022-04-15 18:17:36 +0200

    .

--- Attempt1/src/main/java/com/SafeWebDev/attempt/Controllers/ControllerBasic.java
+++ Attempt1/src/main/java/com/SafeWebDev/attempt/Controllers/ControllerBasic.java
@@ -9,10 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 
@@ -24,6 +21,9 @@ public class ControllerBasic {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private CommentService commentService;
 
 
     /*public ControllerBasic() {  //initializing the default products

ca4aeabc0b200375674442798648248118e48c86
IdkWhatGoesHere <charlieghost938@gmail.com>
2022-04-26 12:38:47 +0200

    Login and register
    esas dos cosas hechas

--- Attempt1/src/main/java/com/SafeWebDev/attempt/Models/Respositories/UserRepository.java
+++ Attempt1/src/main/java/com/SafeWebDev/attempt/Models/Respositories/UserRepository.java
@@ -2,11 +2,11 @@ package com.SafeWebDev.attempt.Models.Respositories;
 
 import com.SafeWebDev.attempt.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
-
-    /*@Query("select cart from user")
-    List<Item> getCart();*/
+    @Query("SELECT u FROM User u WHERE u.userName = ?1")
+    public User findByName(String name);
 }
